dc3a26a79a5837a7ba97a54ac76a9127f37374e8
kiranudhan <udhankiran62@gmail.com>
2023-01-05 13:41:29 +0530

    [Kiran Udhan] added the welcome message to AddressBookSys file

---
+++ src/com_bridgelabz_address_book_system/AddressBookSystem.java
@@ -0,0 +1,11 @@
+package com_bridgelabz_address_book_system;
+
+public class AddressBookSystem {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Address Book Management System");
+
+	}
+
+}
